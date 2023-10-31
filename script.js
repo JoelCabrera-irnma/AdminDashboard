93541e46c6ed46856df3e8872a7cbb51a2ea6c1e
JoelCabrera-irnma <140646912+JoelCabrera-irnma@users.noreply.github.com>
2023-10-31 00:33:37 -0300

    Script link and simple transition Css to some Html element

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector('.sq1').addEventListener('click', function() {
+    // Redirecciona a la URL deseada al hacer clic en el botón
+    window.location.href = 'https://joelcabrera-irnma.github.io/odin-recipes/';
+});
+document.querySelector('.sq2').addEventListener('click', function() {
+    // Redirecciona a la URL deseada al hacer clic en el botón
+    window.location.href = 'https://joelcabrera-irnma.github.io/flexbox-pageweb-joe/';
+});
+document.querySelector('.sq3').addEventListener('click', function() {
+    // Redirecciona a la URL deseada al hacer clic en el botón
+    window.location.href = 'https://joelcabrera-irnma.github.io/PaperRockandScissor/';
+});
+document.querySelector('.sq4').addEventListener('click', function() {
+    // Redirecciona a la URL deseada al hacer clic en el botón
+    window.location.href = 'https://joelcabrera-irnma.github.io/Etch-a-Sketch-Joe/';
+});
+document.querySelector('.sq5').addEventListener('click', function() {
+    // Redirecciona a la URL deseada al hacer clic en el botón
+    window.location.href = 'https://joelcabrera-irnma.github.io/Calculator-JS/';
+});
+document.querySelector('.sq6').addEventListener('click', function() {
+    // Redirecciona a la URL deseada al hacer clic en el botón
+    window.location.href = 'https://joelcabrera-irnma.github.io/Project-Sign-up_Form/';
+});
